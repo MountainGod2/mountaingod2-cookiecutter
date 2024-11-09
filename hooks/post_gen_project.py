117b8215595f63048bec367fd57862a4ea809c8a
MountainGod2 <admin@reid.ca>
2024-11-09 10:30:55 -0700

    feat: Enhance post-generation script to include directory change, linting, and default branch renaming

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,11 +9,20 @@ subprocess.run(["uv", "venv", "--python={{cookiecutter.python_version}}"])
 # Sync dependencies
 subprocess.run(["uv", "sync"])
 
+# Change directory to the project directory
+os.chdir("{{cookiecutter.project_slug}}")
+
+# Run ruff lint
+subprocess.run(["uv", "run", "ruff", "check", "--fix"])
+
 # Initialize git repository
 subprocess.run(["git", "init"])
 
 # Add all files to git
 subprocess.run(["git", "add", "."])
+
+# Rename the default branch to main
+subprocess.run(["git" "branch" "-m" "main"])
 
 # Commit all files
 subprocess.run(["git", "commit", "-m", "chore: Initial commit"])
