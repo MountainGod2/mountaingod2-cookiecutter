9e419c0e9e00d01469cd1658ca79d4f6380d75af
MountainGod2 <admin@reid.ca>
2024-11-08 20:08:58 -0700

    fix: Remove module name validation regex from pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,11 +1,8 @@
 import sys
-import re
 from packaging import version
 from importlib import metadata
 
 MIN_CC_VERSION = "2.0.0"
-
-MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
 # assert cookiecutter >= 2.0.0
 cc_version = version.parse(metadata.version("cookiecutter"))
@@ -17,12 +14,4 @@ if cc_version < min_version:
         f"\tpip install 'cookiecutter>=2', OR\n"
         f"\tconda install 'cookiecutter>=2'"
     )
-    sys.exit(1)
-
-module_name = "{{ cookiecutter.package_name }}"
-
-if not re.match(MODULE_REGEX, module_name):
-    print("ERROR: %s is not a valid Python module name!" % module_name)
-
-    # exits with status 1 to indicate failure
     sys.exit(1)
