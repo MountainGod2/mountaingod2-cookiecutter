c51e119cb1717a0b0c57eba1a57040d0c625f81b
MountainGod2 <admin@reid.ca>
2024-11-09 11:00:49 -0700

    feat: Enhance git initialization in post-generation script with branch management and success message

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -10,13 +10,29 @@ subprocess.run(["uv", "sync"])
 # Run ruff lint
 subprocess.run(["uv", "run", "ruff", "check", "--fix"])
 
-# Create git repository add gh-pages branch and set default branch to main
+# Initialize git repository
 subprocess.run(["git", "init"])
-subprocess.run(["git", "checkout", "-b", "gh-pages"])
-subprocess.run(["git", "checkout", "-b", "main"])
+
+# Add all files to git
 subprocess.run(["git", "add", "."])
-subprocess.run(["git", "commit", "-m", "Initial commit"])
+
+# Rename the default branch to main
 subprocess.run(["git", "branch", "-m", "main"])
 
-# Print message
-print("Project initialized!")
+# Create gh-pages branch
+subprocess.run(["git", "checkout", "--orphan", "gh-pages"])
+
+# Remove all files from gh-pages branch
+subprocess.run(["git", "rm", "-rf", "."])
+
+# Commit changes to gh-pages branch
+subprocess.run(["git", "commit", "--allow-empty", "-m", "Initial commit"])
+
+# Switch back to main branch
+subprocess.run(["git", "checkout", "main"])
+
+# Commit changes to main branch
+subprocess.run(["git", "commit", "-m", "Initial commit"])
+
+# Print success message
+print("Initialized git repository and synced dependencies.")
