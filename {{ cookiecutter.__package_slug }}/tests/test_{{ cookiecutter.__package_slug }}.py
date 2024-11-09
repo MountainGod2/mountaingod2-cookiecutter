35a5269a91fb36e3b7b00131768723b3eabb5235
MountainGod2 <admin@reid.ca>
2024-11-09 10:17:19 -0700

    fix: Suppress unused import warning in test file

--- {{ cookiecutter.__package_slug }}/tests/test_{{ cookiecutter.__package_slug }}.py
+++ {{ cookiecutter.__package_slug }}/tests/test_{{ cookiecutter.__package_slug }}.py
@@ -1,2 +1,2 @@
 """Tests for {{ cookiecutter.__package_slug }}."""
-from {{ cookiecutter.__package_slug }} import {{ cookiecutter.__package_slug }}
+from {{ cookiecutter.__package_slug }} import {{ cookiecutter.__package_slug }}  # noqa: F401
